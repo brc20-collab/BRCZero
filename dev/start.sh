bacb1befa76ad2ef78e6b190f7894e19b6f2e40d
LeoGuo621 <335209779@qq.com>
2023-12-22 11:42:56 +0800

    Update start.sh

--- dev/start.sh
+++ dev/start.sh
@@ -31,8 +31,8 @@ run() {
       --dynamic-gp-mode=2 \
       --consensus.timeout_commit 4000ms \
       --consensus.create_empty_blocks=false \
-      --consensus.start_btc_height=120 \
-      --zero-data-url="http://0.0.0.0/api/v1/crawler/zeroindexer/" \
+      --consensus.start_btc_height=779831 \
+      --zero-data-url="http://127.0.0.1:81/api/v1/crawler/zeroindexer/" \
       --tree-enable-async-commit=false \
       --enable-gid \
       --fast-query=false \
